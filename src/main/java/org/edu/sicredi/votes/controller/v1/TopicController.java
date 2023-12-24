01f736eafe3e5f1257e36cc5b9a879255493f313
Lucas De A. R. Cascão <lucas_cascao_@outlook.com>
2023-12-24 01:57:55 -0300

    Inserido Swagger para disponibilizar a documentação dos endpoints

--- src/main/java/org/edu/sicredi/votes/controller/v1/TopicController.java
+++ src/main/java/org/edu/sicredi/votes/controller/v1/TopicController.java
@@ -1,13 +1,22 @@
 package org.edu.sicredi.votes.controller.v1;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.edu.sicredi.votes.builder.TopicBuilder;
 import org.edu.sicredi.votes.domain.model.TopicVotesCountResult;
 import org.edu.sicredi.votes.domain.request.CreateTopicRequest;
 import org.edu.sicredi.votes.domain.response.CreateTopicResponse;
-import org.edu.sicredi.votes.domain.response.TopicVotesCountResultResponse;
+import org.edu.sicredi.votes.domain.response.ErrorMessageResponse;
+import org.edu.sicredi.votes.domain.response.TopicVotesResultResponse;
 import org.edu.sicredi.votes.service.TopicService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Topic", description = "Topic management APIs")
 @RestController
 @RequestMapping("/v1/topic")
 @RequiredArgsConstructor
@@ -24,8 +34,18 @@ public class TopicController {
   private final TopicService service;
   private final TopicBuilder topicBuilder;
 
+  @Operation(
+      summary = "Get Topic Voting Result",
+      description = "Endpoint responsible for counting and returning the topic voting results"
+  )
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", content = {
+          @Content(schema = @Schema(implementation = TopicVotesResultResponse.class), mediaType = MediaType.APPLICATION_JSON_VALUE)}),
+      @ApiResponse(responseCode = "500", content = {
+          @Content(schema = @Schema(implementation = ErrorMessageResponse.class), mediaType = MediaType.APPLICATION_JSON_VALUE)})
+  })
   @GetMapping("/{topicId}")
-  public ResponseEntity<TopicVotesCountResultResponse> getTopicResult(
+  public ResponseEntity<TopicVotesResultResponse> getTopicResult(
       @PathVariable(name = "topicId") String topicId
   ) {
     TopicVotesCountResult topicVotesCountResult = this.service.countVotesByTopic(topicId);
@@ -34,9 +54,19 @@ public class TopicController {
     );
   }
 
+  @Operation(
+      summary = "Create a New Topic",
+      description = "Endpoint responsible for create a new topic for the associates to vote"
+  )
+  @ApiResponses({
+      @ApiResponse(responseCode = "201", content = {
+          @Content(schema = @Schema(implementation = CreateTopicResponse.class), mediaType = MediaType.APPLICATION_JSON_VALUE)}),
+      @ApiResponse(responseCode = "500", content = {
+          @Content(schema = @Schema(implementation = ErrorMessageResponse.class), mediaType = MediaType.APPLICATION_JSON_VALUE)})
+  })
   @PostMapping
-  public ResponseEntity<CreateTopicResponse> createTopic (
-      @RequestBody CreateTopicRequest createTopicRequest
+  public ResponseEntity<CreateTopicResponse> createTopic(
+      @Valid @RequestBody CreateTopicRequest createTopicRequest
   ) {
     String topicId = this.service.createNewTopic(
         createTopicRequest.getName(),
@@ -50,6 +80,16 @@ public class TopicController {
     );
   }
 
+  @Operation(
+      summary = "Enable The Topic to Receive Votes",
+      description = "Endpoint responsible for enabling the topic to receive votes from the associates"
+  )
+  @ApiResponses({
+      @ApiResponse(responseCode = "204", content = {
+          @Content(schema = @Schema(implementation = CreateTopicResponse.class), mediaType = MediaType.APPLICATION_JSON_VALUE)}),
+      @ApiResponse(responseCode = "500", content = {
+          @Content(schema = @Schema(implementation = ErrorMessageResponse.class), mediaType = MediaType.APPLICATION_JSON_VALUE)})
+  })
   @PostMapping("/{topicId}")
   public ResponseEntity<Void> initializeVoting (
       @PathVariable(name = "topicId") String topicId
--- src/main/java/org/edu/sicredi/votes/controller/v1/VotesController.java
+++ src/main/java/org/edu/sicredi/votes/controller/v1/VotesController.java
@@ -1,16 +1,25 @@
 package org.edu.sicredi.votes.controller.v1;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.edu.sicredi.votes.domain.enums.VoteOptionEnum;
 import org.edu.sicredi.votes.domain.request.RegisterVoteRequest;
+import org.edu.sicredi.votes.domain.response.ErrorMessageResponse;
 import org.edu.sicredi.votes.service.VoteService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Votes", description = "Votes management APIs")
 @RestController
 @RequestMapping("/v1/votes")
 @RequiredArgsConstructor
@@ -18,6 +27,15 @@ public class VotesController {
 
   private final VoteService service;
 
+  @Operation(
+      summary = "Register Individual Vote",
+      description = "Endpoint responsible for register the associate's vote"
+  )
+  @ApiResponses({
+      @ApiResponse(responseCode = "201"),
+      @ApiResponse(responseCode = "500", content = {
+          @Content(schema = @Schema(implementation = ErrorMessageResponse.class), mediaType = MediaType.APPLICATION_JSON_VALUE)})
+  })
   @PostMapping
   public ResponseEntity<Void> registerIndividualVote(
       @RequestBody RegisterVoteRequest request) {
